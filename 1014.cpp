e76a5a92babb06db219cbed6f8c224966151dccf
ABCGong <1326678604@qq.com>
2021-10-16 01:07:43 +0800

    记录大无语事件

--- 1014.cpp
+++ 1014.cpp
@@ -4,26 +4,26 @@ using namespace std;
 int main(){
 	string day[7] = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
 	string a, b, c, d;
-	int t;
+	int next;
 	cin >> a >> b >> c >> d;
 	for(int i = 0; i < a.length() && i <b.length() ; i++){
-		if(a[i] == b[i] && a[i] >= 'A' && a[i] <= 'N'){
+		if(a[i] == b[i] && a[i] >= 'A' && a[i] <= 'O'){
 			cout << day[a[i] - 'A'] << " ";
-			t = i;
+			next = i;
 			break;
 		}
 	}
-	for(int i = t + 1; i < a.length() && i < b.length(); i++){
+	for(int i = next + 1; i < a.length() && i < b.length(); i++){
 		if(a[i] == b[i] ){
 			if(a[i]>='0'&&a[i]<='9') printf("%02d:",a[i]-'0');
-			else if(a[i] <='N'&&a[i]>='A') printf("%02d:", a[i]-'A'+10);
+			else if(a[i] <='O'&&a[i]>='A') printf("%02d:", a[i]-'A'+10);
 			break;
 		}
 	}
 	for(int i = 0; i < c.length() && i < d.length(); i++){
 		if(d[i] == c[i] &&  ((d[i]>='A'&d[i]<='Z')||(d[i]<='z'&&d[i]>='a'))){
 			printf("%02d", i);
-			return 0;
+			break;
 		}
 	}
 	return 0;
